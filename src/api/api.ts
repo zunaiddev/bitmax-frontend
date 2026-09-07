4c360532e98aee25bef443cd8ad7d8031340d69e
zunaiddev <ja7667924@gmail.com>
2026-09-07 22:08:00 +0530

    updated api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,14 +1,9 @@
 import axios from "axios";
 
-// const BASE_URL = import.meta.env.VITE_BASE_URL ?? "";
+const BASE_URL: string = import.meta.env.VITE_BASE_URL ?? "";
 
 const publicApi = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: BASE_URL,
 });
-
-// const privateApi: AxiosInstance = axios.create({
-//     baseURL: BASE_URL,
-//     withCredentials: true,
-// });
 
 export {publicApi};
